f8ad168bea9ac234bbe19520ec0f1706bb9e09d7
Dwarkesh Haldankar <dwarkesh.haldankar@yahoo.com>
2018-08-24 19:34:19 +0530

    Weekend Going Home!

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    #path('', myblog.views.index , name='index' ),
+    path('', include('myblog.urls')), #Though I doubt if this approach is right
+    path('blog/', include('myblog.urls')),
     path('admin/', admin.site.urls),
+
 ]
---
+++ blog/myblog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import datetime
+# Create your models here.
+
+class Author(models.Model):
+    author_name = models.TextField(max_length=50)
+    author_city = models.TextField(max_length=15)
+
+class Article(models.Model):
+    title = models.TextField(max_length=100)
+    
